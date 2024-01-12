860f899a363716d04e29e7a03fde50ae6ef2cf81
hoctiengKhmer <viasanaokok@gmail.com>
2024-01-12 15:11:36 +0700

    new ui

---
+++ page/nguyen_am/nguyen_am.js
@@ -0,0 +1,77 @@
+var section = document.getElementById("consonant_box")
+let vowel = ["អ","ា", "ិ", "ី", "ឹ", "ឺ", "ុ", "ូ", "ួ", "ើ", "ឿ", "ៀ", "េ", "ែ", "ៃ", "ោ", "ៅ", "ុំ", "ំ", "ាំ", "ះ","ិះ", "ុះ", "េះ", "ោះ"]
+const WORDS = [["កា", "កី"],
+             ["ខា", "​ខួ"]];
+
+
+function createCard(letters){
+    for ( i in letters){
+        card = document.getElementById("card_"+i)||document.createElement('div');
+        card.style.display = 'unset';
+        card.className = "card";
+        card.id = "card_" + i;
+        card.innerHTML = `<h1 class="tiltle" onclick="Learn(${i})">${letters[i]}</h1>`;
+        card.style.backgroundSize = 'cover';
+        card.style.backgroundImage = 'url(https://github.com/viaasana/tu_luyen_chu_viet_tieng_khmer_nam_bo/blob/main/image/phu_am/'+ i + '.png?raw=true)'
+
+        section.appendChild(card);
+    }
+    
+}
+
+createCard(vowel);
+
+
+function Learn(letter){
+    document.getElementById("containner").style.display = 'none';
+
+
+    /*    <div class="containner2">
+        <button class="escButton">X</button>
+        <h1 calss="title">ក</h1>
+        <button class="speakerButton"><i class="material-icons">speaker</i></button>
+        <h3 calss="words"><i>កា</i>,<i>កី</i><i>កា</i>,<i>កី</i><i>កា</i>,<i>កី</i><i>កា</i>,<i>កី</i><i>កា</i>,<i>កី</i><i>កា</i>,<i>កី</i></h3>
+        <video src="" class="video"></video>
+        
+    </div>*/
+    var container = document.getElementById("containner2");
+    container.style.display = 'unset';
+    var escButton =document.getElementsByClassName("escButton")[0] || document.createElement("button");
+    escButton.className = "escButton";
+    escButton.innerHTML = "X";
+    escButton.onclick = function (){escLearn()};
+    var title = document.getElementsByClassName("title")[0] ||document.createElement('h1');
+    title.className = "title";
+    title.innerHTML = vowel[letter];
+    var speakerButton = document.getElementsByClassName("speakerButton")[0] ||document.createElement("button");
+    speakerButton.className = "speakerButton";
+    speakerButton.innerHTML = '<i class="material-icons">speaker</i>';
+    speakerButton.onclick = function(){playSound(letter)}
+    var words = document.getElementsByClassName("words")[0] ||document.createElement("h3");
+    words.className = "words";
+    var string = `<i>${WORDS[letter]}</i>`;
+    words.innerHTML = string;
+    var video =  document.getElementsByClassName("video")[0] ||document.createElement("video");
+    video.className = "video";
+    video.src = 'https://github.com/viaasana/tu_luyen_chu_viet_tieng_khmer_nam_bo/blob/main/video/vowel_video/'+ letter + '.mp4?raw=true';
+    video.autoplay = true;
+    video.loop = true;  
+    container.appendChild(escButton);
+    container.appendChild(title);
+    container.appendChild(speakerButton);
+    container.appendChild(words);
+    container.appendChild(video);
+}
+
+
+function escLearn(){
+    document.getElementById("containner").style.display = 'flex';
+    document.getElementById('containner2').style.display = 'none';
+}
+
+function playSound(letter){
+    var audio = document.getElementById('audio') || document.createElement('audio');
+    audio.src ='https://github.com/viaasana/tu_luyen_chu_viet_tieng_khmer_nam_bo/blob/main/sound/NGUYEN_AM/Recording%20(' + parseFloat(letter  + 35)+ ').m4a?raw=true'
+    audio.id = 'audio';
+    audio.autoplay = true;
+}
--- script.js
+++
@@ -1,7 +0,0 @@
-function openWriting(){
-    window.location = "./page/writing/writing_index.html";
-}
-
-function openExercises(){
-    window.location = "./page/tracNghiem/tracNghiem.html";
-}
